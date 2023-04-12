705c64df4efc63e7e480c1f573357617c1496519
JHacin <jan.hacin@gmail.com>
2023-04-12 22:01:27 +0200

    update npm packages

--- features/cat-sponsorship/constants/index.ts
+++ features/cat-sponsorship/constants/index.ts
@@ -1,14 +1,13 @@
 import { isAgreedToTermsValidation, NUMBER_INPUT_MAX } from "@/forms/constants";
 import * as yup from "yup";
 
-// noinspection JSUnusedGlobalSymbols
 export const catFormValidation = {
   paramsStep: {
     is_gift: yup.boolean(),
     monthly_amount: yup.number().integer().min(5).max(NUMBER_INPUT_MAX).required(),
     requested_duration: yup.number().when("is_gift", {
       is: true,
-      then: (schema) => schema.integer().positive().max(NUMBER_INPUT_MAX).nullable(true),
+      then: (schema) => schema.integer().positive().max(NUMBER_INPUT_MAX).nullable(),
       otherwise: (schema) => schema.strip(),
     }),
     wants_direct_debit: yup.boolean(),
